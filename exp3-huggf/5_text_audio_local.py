d34d0bed6768e79972b742fc38486e25a329c63a
KP <head.iie@vnrvjiet.in>
2024-07-02 15:16:40 +0530

    Text to audio converstion using online model  #7

---
+++ exp3-huggf/5_text_audio_local.py
@@ -0,0 +1,19 @@
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from datasets import load_dataset
+import torch
+import soundfile as sf
+from datasets import load_dataset
+
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+inputs = processor(text="VNRVJIET currently supports mentorship for B.Tech students through a system where each faculty member is assigned 20 students for the entirety of their 4-year course. The current process involves students logging their mentorship activities in a physical book, which mentors later transcribe into the EduPrime online portal. This approach has several drawbacks", return_tensors="pt")
+
+# load xvector containing speaker's voice characteristics from a dataset
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+sf.write("speech.wav", speech.numpy(), samplerate=16000)
